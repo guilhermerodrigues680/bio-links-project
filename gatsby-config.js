7a2da4fd03dafc0c0262e4169f56733f5bb3421c
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-07-09 15:15:03 -0300

    cria lista de links usando cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
   ],
 };
---
+++ src/components/Layout.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { Helmet } from "react-helmet";
+
+function Layout({ children, title = "" }) {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          siteUrl
+          description
+        }
+      }
+    }
+  `);
+  console.debug(data);
+
+  return (
+    <>
+      <Helmet>
+        <html lang="pt-BR" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{`${title} | ${data?.site?.siteMetadata?.title}`}</title>
+
+        {/* https://favicon.io/favicon-converter/ */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Helmet>
+
+      <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+        {children}
+      </div>
+    </>
+  );
+}
+
+export default Layout;

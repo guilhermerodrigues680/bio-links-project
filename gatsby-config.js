33020ac892620be07b1e6ce785d02c7f47123acf
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-07-09 08:14:47 -0300

    remove configurações de deploy netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,5 @@ module.exports = {
     title: `Projeto Bio Links`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    `gatsby-plugin-netlify-cms`,
-    "gatsby-plugin-netlify", // make sure to keep it last in the array
-  ],
+  plugins: [`gatsby-plugin-netlify-cms`],
 };
